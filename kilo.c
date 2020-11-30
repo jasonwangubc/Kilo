222e89c810f0d510f2d2f031a96913f6e7f6b70f
jasonwang <wyc_jason@yahoo.com>
2020-11-29 22:54:45 -0800

    add turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -2,22 +2,25 @@
 #include <termios.h>
 #include <unistd.h>
 
-struct termios original_termios;
+struct termios original_terminal_attributes;
 
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_terminal_attributes);
 }
 
 void enableRawMode(){
 
-    tcgetattr(STDIN_FILENO, &original_termios);
+    tcgetattr(STDIN_FILENO, &original_terminal_attributes);
+    
+    // registers a function to be called when main returns or exit() is called
     atexit(disableRawMode);
 
-    struct termios raw = original_termios;
+    struct termios raw_terminal_attributes = original_terminal_attributes;
 
-    raw.c_lflag &= ~(ECHO);
+    // ECHO and ICANON are bitflags, bitwise NOT plus AND sets these flags to 0 in c_lflag
+    raw_terminal_attributes.c_lflag &= ~(ECHO | ICANON);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_terminal_attributes);
 }
 
 int main(){
